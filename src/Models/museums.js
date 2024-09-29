ceb0c87c305d3de0b1b5aad1e3e344eeee32db41
HaidyElkenawy <160390133+HaidyElkenawy@users.noreply.github.com>
2024-09-29 20:57:53 +0300

    Haidy

---
+++ src/Models/museums.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const museumsSchema = new Schema({
+    Name : {
+        type: String,
+        required : true,
+    },
+
+    description : {
+        type: String,
+        required : true,
+    },
+
+    pictures : {
+        type: String,
+        required : true,
+    },
+    location : {
+        type: String,
+        required : true,
+    },
+    Country : {
+        type: String,
+        required : true,
+    },
+
+    Opening_Hours: {
+        type: String,
+        required : true,
+    },
+
+    S_Tickets_Prices : {
+        type: Number,
+        required : true,
+    },
+    F_Tickets_Prices : {
+        type: Number,
+        required : true,
+    },
+    N_Tickets_Prices : {
+        type: Number,
+        required : true,
+    },
+
+},{ versionKey: false });
+
+const Meuseum = mongoose.model('Meuseum', museumsSchema);
+module.exports = Meuseum;
