f51803069991b1d3c359f585b284f6374911ee4b
HaidyElkenawy <160390133+HaidyElkenawy@users.noreply.github.com>
2024-09-29 21:14:49 +0300

    Haidy

---
+++ src/Models/historical_places.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const historical_placesSchema = new Schema({
+    Name : {
+        type: String,
+        required : true,
+    },
+    Description : {
+        type: String,
+        required : true,
+    },
+    Pictures : {
+        type: String,
+       // required : true,
+    },
+    Location : {
+        type: String,
+        required : true,
+    },
+    Country : {
+        type: String,
+        required : true,
+    },
+    Opens_At : {
+        type: String,
+        required : true,
+    },
+    Closes_At : {
+        type: String,
+        required : true,
+    },
+    
+    S_Ticket_Prices : {
+        type: Number,
+        required : true,
+    },
+    F_Ticket_Prices : {
+        type: Number,
+        required : true,
+    },
+    N_Ticket_Prices : {
+        type: Number,
+        required : true,
+    },
+
+},{ versionKey: false });
+const Historical_Place = mongoose.model('historical_places', historical_placesSchema);
+module.exports = Historical_Place;
