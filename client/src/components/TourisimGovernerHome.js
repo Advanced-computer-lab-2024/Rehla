54800bfe9750dce53495201c967a59f84cec9722
NORA-262 <168011617+NORA-262@users.noreply.github.com>
2024-10-07 18:35:17 +0300

    nourhan

--- client/src/components/TourisimGovernerHome.js
+++ client/src/components/TourisimGovernerHome.js
@@ -297,11 +297,59 @@ const TourisimGovernerHome = () => {
 
             {/* Display Responses */}
             {museumResponse && (
-                <div>
-                    <h3>Museum Response:</h3>
-                    <pre>{JSON.stringify(museumResponse, null, 2)}</pre>
-                </div>
-            )}
+    <div>
+        <h3>Museum Response:</h3>
+        <table style={{ borderCollapse: 'collapse', width: '100%' }}>
+            <tbody>
+                <tr>
+                    <th style={{ border: '1px solid black', padding: '8px' }}>Name</th>
+                    <td style={{ border: '1px solid black', padding: '8px' }}>{museumResponse.data.Name}</td>
+                </tr>
+                <tr>
+                    <th style={{ border: '1px solid black', padding: '8px' }}>Description</th>
+                    <td style={{ border: '1px solid black', padding: '8px' }}>{museumResponse.data.description}</td>
+                </tr>
+                <tr>
+                    <th style={{ border: '1px solid black', padding: '8px' }}>Pictures</th>
+                    <td style={{ border: '1px solid black', padding: '8px' }}>{museumResponse.data.pictures}</td>
+                </tr>
+                <tr>
+                    <th style={{ border: '1px solid black', padding: '8px' }}>Location</th>
+                    <td style={{ border: '1px solid black', padding: '8px' }}>{museumResponse.data.location}</td>
+                </tr>
+                <tr>
+                    <th style={{ border: '1px solid black', padding: '8px' }}>Country</th>
+                    <td style={{ border: '1px solid black', padding: '8px' }}>{museumResponse.data.Country}</td>
+                </tr>
+                <tr>
+                    <th style={{ border: '1px solid black', padding: '8px' }}>Opening Hours</th>
+                    <td style={{ border: '1px solid black', padding: '8px' }}>{museumResponse.data.Opening_Hours}</td>
+                </tr>
+                <tr>
+                    <th style={{ border: '1px solid black', padding: '8px' }}>Standard Ticket Prices</th>
+                    <td style={{ border: '1px solid black', padding: '8px' }}>{museumResponse.data["S_Tickets_Prices "]}</td>
+                </tr>
+                <tr>
+                    <th style={{ border: '1px solid black', padding: '8px' }}>Foreign Ticket Prices</th>
+                    <td style={{ border: '1px solid black', padding: '8px' }}>{museumResponse.data["F_Tickets_Prices "]}</td>
+                </tr>
+                <tr>
+                    <th style={{ border: '1px solid black', padding: '8px' }}>National Ticket Prices</th>
+                    <td style={{ border: '1px solid black', padding: '8px' }}>{museumResponse.data["N_Tickets_Prices "]}</td>
+                </tr>
+                <tr>
+                    <th style={{ border: '1px solid black', padding: '8px' }}>Tag</th>
+                    <td style={{ border: '1px solid black', padding: '8px' }}>{museumResponse.data.Tag}</td>
+                </tr>
+                <tr>
+                    <th style={{ border: '1px solid black', padding: '8px' }}>Created By</th>
+                    <td style={{ border: '1px solid black', padding: '8px' }}>{museumResponse.data.Created_By}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+)}
+
             {placeResponse && (
                 <div>
                     <h3>Historical Place Response:</h3>
