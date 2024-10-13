b138ce85a01793182dc27a49227de94e6f2a8b91
rana-selimm <rana.selim@student.guc.edu.eg>
2024-10-13 16:18:49 +0300

    ranaaa

---
+++ src/Models/terms_and_conditions.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const terms_and_conditionsSchema = new Schema({
+    Email : {
+        type: String,
+        required: true,
+    },
+
+    
+    Type : {
+        type: String,
+        enum : ['Tour_Guide', 'Advirtisor', 'Seller'],
+        required: true,
+    },
+    Terms_and_Conditions : {
+        type: String,
+
+    },
+
+    Paid: {
+        type: Boolean,
+        required : true,
+    },
+
+},{ versionKey: false });
+
+const Terms_and_Conditions = mongoose.model('Terms_and_Conditions', terms_and_conditionsSchema);
+module.exports = Terms_and_Conditions;
