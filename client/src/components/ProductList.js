5d2cd29ba5acf3d0ae2d62511703b3dd5bb3f683
HaidyElkenawy <160390133+HaidyElkenawy@users.noreply.github.com>
2024-11-01 14:11:46 +0200

    Haidy

--- client/src/components/ProductList.js
+++ client/src/components/ProductList.js
@@ -1,7 +1,53 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../images/logo.png';
 import { getProducts, getProductsSortedByRating } from '../services/api'; // Import the search API call
+const Header = () => (
+    <div className="NavBar">
+    <img src={logo} alt="Logo" />
+    <nav className="main-nav">
+        <ul className="nav-links">
+            <Link to="/TouristHome">Home</Link>
+            <Link to="/MyEvents">Events/Places</Link>
+        </ul>
+    </nav>
 
+    <nav className="signing">
+        <Link to="/TouristHome/TouristProfile">My Profile</Link>
+    </nav>
+</div>
+);
+
+
+const Footer = () => (
+    <footer className="bg-brandBlue shadow dark:bg-brandBlue m-0">
+    <div className="w-full mx-auto md:py-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+            <a href="/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                <img src={logo} className="w-12" alt="Flowbite Logo" />
+            </a>
+            <div className="flex justify-center w-full">
+                <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400 -ml-14">
+                    <li>
+                        <a href="/" className="hover:underline me-4 md:me-6">About</a>
+                    </li>
+                    <li>
+                        <a href="/" className="hover:underline me-4 md:me-6">Privacy Policy</a>
+                    </li>
+                    <li>
+                        <a href="/" className="hover:underline me-4 md:me-6">Licensing</a>
+                    </li>
+                    <li>
+                        <a href="/" className="hover:underline">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="/" className="hover:underline">Rehla™</a>. All Rights Reserved.</span>
+    </div>
+</footer>
+);
 const ProductList = () => {
     const [products, setProducts] = useState([]);
     const [loading, setLoading] = useState(true);
@@ -89,62 +135,65 @@ const ProductList = () => {
     }
 
     return (
+        <div>
         <div className="container mx-auto px-4 py-10">
-            <h1 className="text-4xl font-bold text-center mb-8">Product List</h1>
+            <Header/>
+            <h1 className="text-4xl font-bold text-center mb-8">Products List</h1>
 
             {/* Search, Filter, and Sort Section */}
             <div className="mb-8">
-                <form onSubmit={handleSearchProducts} className="flex items-center space-x-4">
+                <form onSubmit={handleSearchProducts} className="flex flex-col items-center">
                     {/* Search */}
-                    <input
-                        type="text"
-                        placeholder="Search by product name"
-                        value={searchTerm}
-                        onChange={(e) => setSearchTerm(e.target.value)}
-                        className="border border-gray-300 rounded-lg px-4 py-2 w-80"
-                    />
-                    <button type="submit" className="bg-blue-500 text-white px-6 py-2 rounded-lg">Search</button>
-
-                    {/* Filter */}
-                    <div className="flex space-x-4 items-center">
-                        <div className="flex space-x-2 items-center">
-                            <label className="font-medium">Min Price:</label>
-                            <input
-                                type="number"
-                                name="minPrice"
-                                value={priceFilters.minPrice}
-                                onChange={handleFilterChange}
-                                className="border border-gray-300 rounded-lg px-4 py-2 w-24"
-                            />
-                        </div>
-                        <div className="flex space-x-2 items-center">
-                            <label className="font-medium">Max Price:</label>
-                            <input
-                                type="number"
-                                name="maxPrice"
-                                value={priceFilters.maxPrice}
-                                onChange={handleFilterChange}
-                                className="border border-gray-300 rounded-lg px-4 py-2 w-24"
-                            />
-                        </div>
-                        <button 
-                            type="button" 
-                            onClick={handleFilterProducts} 
-                            className="bg-green-500 w-36 text-white px-6 py-2 rounded-lg"
-                        >
-                            Apply Filter
-                        </button>
+                    <div className="flex justify-center mb-4 w-full">
+                        <input
+                            type="text"
+                            placeholder="Search by product name"
+                            value={searchTerm}
+                            onChange={(e) => setSearchTerm(e.target.value)}
+                            className="border border-gray-300 rounded-full px-6 py-3 w-full max-w-3xl" // Increased max-width
+                        />
+                        <button type="submit" className="bg-brandBlue text-white px-6 py-3 rounded-full ml-2">Search</button>
                     </div>
+                </form>
 
-                    {/* Sort */}
+                {/* Filter and Sort Section */}
+                <div className="flex space-x-4 items-center justify-center">
+                    <div className="flex space-x-2 items-center">
+                        <label className="font-medium">Min Price:</label>
+                        <input
+                            type="number"
+                            name="minPrice"
+                            value={priceFilters.minPrice}
+                            onChange={handleFilterChange}
+                            className="border border-gray-300 rounded-lg px-4 py-2 w-24"
+                        />
+                    </div>
+                    <div className="flex space-x-2 items-center">
+                        <label className="font-medium">Max Price:</label>
+                        <input
+                            type="number"
+                            name="maxPrice"
+                            value={priceFilters.maxPrice}
+                            onChange={handleFilterChange}
+                            className="border border-gray-300 rounded-lg px-4 py-2 w-24"
+                        />
+                    </div>
+                    <button 
+                        type="button" 
+                        onClick={handleFilterProducts} 
+                        className="bg-brandBlue w-36 text-white px-6 py-2 rounded-lg"
+                     >
+                        Apply Filter
+                    </button>
+                    
                     <button 
                         type="button" 
                         onClick={handleSortProductsByRating} 
-                        className="bg-indigo-500 w-52 text-white px-6 py-2 rounded-lg"
+                        className="bg-logoOrange w-52 text-white px-6 py-2 rounded-lg"
                     >
                         Sort by Rating
                     </button>
-                </form>
+                </div>
             </div>
 
             {/* Conditionally Render Products */}
@@ -233,6 +282,9 @@ const ProductList = () => {
                     ))}
                 </div>
             )}
+
+        </div>
+        <Footer />
         </div>
     );
 };
