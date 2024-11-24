bdf43d831ec9096bcbcaee8ce4d6aed7a72738c0
amrmegahed03 <amr.khaled.mostafa3@gmail.com>
2024-11-24 18:59:39 +0200

    Amr

--- src/Routes/Controller.js
+++ src/Routes/Controller.js
@@ -2865,55 +2865,10 @@ const creatTouristItenrary= async(req,res)=>{
             { $inc: { Booked: 1, Empty_Spots: -1 } } // Increment Booked and decrement Empty_Spots
         );
 
-        //pay for the itinrary
-        const itinerary = await itinerarym.findOne({ Itinerary_Name });
-
-        //check that the tourist has enough money in their wallet
-        if (touristExists.Wallet < itinerary.Tour_Price) {
-            return res.status(400).json({ error: 'Insufficient funds in your wallet to book this itinerary.' });
-        }
-
-        // Deduct the itinerary price from the tourist's wallet balance
-        touristExists.Wallet -= itinerary.Tour_Price;
-
-         // Calculate points based on the level
-         let points = 0;
-         switch (touristExists.Badge) {
-             case 'Level 1':
-                 points = itinerary.Tour_Price * 0.5;
-                 break;
-             case 'Level 2':
-                 points = itinerary.Tour_Price * 1;
-                 break;
-             case 'Level 3':
-                 points = itinerary.Tour_Price * 1.5;
-                 break;
-             default:
-                 points = 0;
-         }
- 
-         // Add points to the tourist's account
-         touristExists.Points += points;
-         await touristExists.save();
- 
-         // Update the tourist's badge based on the new points
-         if (touristExists.Points <= 100000) {
-            touristExists.Badge = 'Level 1';
-         } else if (touristExists.Points <= 500000) {
-            touristExists.Badge = 'Level 2';
-         } else {
-            touristExists.Badge = 'Level 3';
-         }
- 
-         await touristExists.save(); 
-
-         //mark the itinerary as paid
-            savedTouristItenrary.Paid = true;
-            await savedTouristItenrary.save();
+        await savedTouristItenrary.save();
 
         res.status(201).json({ message: 'Tourist itinerary created successfully', Tourist_Itinerary: savedTouristItenrary });
-
-
+        
     } catch (error) {
         console.error("Error details:", error.message, error.stack); // Log full error details
         res.status(500).json({ error: 'Error creating tourist itinerary', details: error.message });
@@ -3245,53 +3200,7 @@ const createTouristActivity = async (req, res) => {
             { Booking_Available: false }
         );
 
-        //PAY FOR THE ACTIVITY
-        const activityy = await activity.findOne({ Name: Activity_Name });
-
-        // Check if the tourist has enough money in their wallet
-        if (touristExists.Wallet < activityy.Price) {
-            return res.status(400).json({ error: 'Insufficient funds in your wallet to book this activity.' });
-        }
-
-        // Deduct the activity price from the tourist's wallet balance
-        touristExists.Wallet -= activityy.Price;
-
-        // Calculate points based on the level
-        let points = 0;
-        switch (touristExists.Badge) {
-            case 'Level 1':
-                points = activityy.Price * 0.5;
-                break;
-            case 'Level 2':
-                points = activityy.Price * 1;
-                break;
-            case 'Level 3':
-                points = activityy.Price * 1.5;
-                break;
-            default:
-                points = 0;
-        }
-
-        // Add points to the tourist's account
-        touristExists.Points += points;
-        await touristExists.save();
-
-        // Update the tourist's badge based on the new points
-        if (touristExists.Points <= 100000) {
-            touristExists.Badge = 'Level 1';
-        } else if (touristExists.Points <= 500000) {
-            touristExists.Badge = 'Level 2';
-        } else {
-            touristExists.Badge = 'Level 3';
-        }
-
-        await touristExists.save();
-
-        // Mark the tourist activity as paid
-        savedTouristActivity.Paid = true;
         await savedTouristActivity.save();
-
-        
 
         // Send a response
         res.status(201).json({ message: 'Tourist activity created successfully', tourist_activity: savedTouristActivity });
