fcce1e81382c11b0c965ed2a163d373dcef955d3
HaidyElkenawy <160390133+HaidyElkenawy@users.noreply.github.com>
2024-11-27 20:31:48 +0200

    Haidyy

--- client/src/components/EventDetails.js
+++ client/src/components/EventDetails.js
@@ -25,6 +25,8 @@ const EventDetails = () => {
   const [tourGuideComment, setTourGuideComment] = useState("");
   const [errorTourGuide, setErrorTourGuide] = useState("");
 
+  const [attendedStatus, setAttendedStatus] = useState(null);
+
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -47,6 +49,10 @@ const EventDetails = () => {
         } else {
           throw new Error("Invalid type provided");
         }
+
+        // Fetch the attended status from localStorage
+        const attended = localStorage.getItem(`${selectedType}_${selectedName}_attended`);
+        setAttendedStatus(attended === "true");
       } catch (err) {
         console.error("Error fetching details:", err);
       }
@@ -135,9 +141,7 @@ const EventDetails = () => {
         {[...Array(maxRating)].map((_, index) => (
           <span
             key={index}
-            className={`text-3xl cursor-pointer ${
-              index < currentRating ? "text-yellow-500" : "text-gray-300"
-            }`}
+            className={`text-3xl cursor-pointer ${index < currentRating ? "text-yellow-500" : "text-gray-300"}`}
             onClick={() => onClickHandler(index + 1)}
           >
             ★
@@ -150,31 +154,37 @@ const EventDetails = () => {
   if (!details) return <div>Loading...</div>;
 
   return (
+    <div>
     <div className="min-h-screen flex flex-col items-center">
       <header className="NavBar flex items-center justify-between p-4 bg-brandBlue shadow-md w-full">
         <img src={logo} alt="Logo" className="h-12" />
         <nav className="main-nav">
           <ul className="nav-links flex space-x-6">
-            <Link to="/TouristHome" className="text-white font-medium hover:underline">
-              Home
-            </Link>
+            <Link to="/TouristHome" className="text-white font-medium hover:underline">Home</Link>
           </ul>
         </nav>
         <nav className="signing">
-          <Link to="/TourGuideHome/TourGuideProfile" className="text-white font-medium hover:underline">
-            My Profile
-          </Link>
+          <Link to="/TourGuideHome/TourGuideProfile" className="text-white font-medium hover:underline">My Profile</Link>
         </nav>
       </header>
 
       <main className="flex-grow flex items-center justify-center mt-32 w-full">
         <div className="p-6 bg-white shadow-lg rounded-lg w-3/4 flex flex-col lg:flex-row">
-          <div className="lg:w-1/3 flex-shrink-0">
+        <div className="lg:w-1/3 flex-shrink-0 flex flex-col items-center">
+            {/* Set a fixed square size for the image */}
             <img
               src={details.Picture}
               alt={details.Name || "Image"}
-              className="w-full h-full object-cover rounded"
+               className="w-72 h-72 object-cover rounded"
             />
+            {/* Display Attended or Not Attended status */}
+            {attendedStatus !== null && (
+              <div className="mt-4 text-center">
+                <h2 className="text-xl font-bold text-gray-800">
+                  Status: {attendedStatus ? "Attended" : "Not Attended"}
+                </h2>
+              </div>
+            )}
           </div>
           <div className="lg:w-2/3 lg:pl-6">
             <h1 className="text-4xl font-bold mb-6 text-left text-gray-800">
@@ -232,57 +242,58 @@ const EventDetails = () => {
                   >
                     Submit Rating
                   </button>
-                  <h2 className="text-xl font-bold text-gray-800 mt-6">Comment on the Tour Guide:</h2>
-                  <textarea
-                    value={tourGuideComment}
-                    onChange={(e) => setTourGuideComment(e.target.value)}
-                    className="w-full p-2 border border-gray-300 rounded-md mt-2"
-                    placeholder="Write your comment for the tour guide..."
-                  />
-                  <button
-                    onClick={handleTourGuideCommentSubmit}
-                    className="mt-4 bg-brandBlue text-white py-2 px-6 rounded-lg hover:bg-logoOrange transition duration-200"
-                  >
-                    Submit Comment
-                  </button>
-                  {errorTourGuide && <p className="text-red-500">{errorTourGuide}</p>}
+                  <div className="mt-6">
+                    <textarea
+                      value={tourGuideComment}
+                      onChange={(e) => setTourGuideComment(e.target.value)}
+                      className="w-full p-2 border border-gray-300 rounded-md mt-2"
+                      placeholder="Leave a comment for the tour guide..."
+                    />
+                    <button
+                      onClick={handleTourGuideCommentSubmit}
+                      className="mt-4 bg-brandBlue text-white py-2 px-6 rounded-lg hover:bg-logoOrange transition duration-200"
+                    >
+                      Submit Comment
+                    </button>
+                  </div>
+                  {errorTourGuide && (
+                    <p className="text-red-500 mt-2">{errorTourGuide}</p>
+                  )}
                 </div>
               )}
             </div>
           </div>
         </div>
       </main>
-
-      <footer className="bg-brandBlue shadow py-6 w-full mt-20">
-        <div className="w-full mx-auto flex flex-col items-center space-y-6">
-          <a href="/" className="flex items-center space-x-3">
-            <img src={logo} className="w-12" alt="Logo" />
-          </a>
-          <ul className="flex flex-wrap justify-center space-x-6 text-sm text-gray-500">
-            <li>
-              <a href="/" className="hover:underline">
-                About
-              </a>
-            </li>
-            <li>
-              <a href="/" className="hover:underline">
-                Privacy Policy
-              </a>
-            </li>
-            <li>
-              <a href="/" className="hover:underline">
-                Licensing
-              </a>
-            </li>
-            <li>
-              <a href="/" className="hover:underline">
-                Contact
-              </a>
-            </li>
-          </ul>
-          <p className="text-sm text-gray-500">© 2023 Rehla™. All Rights Reserved.</p>
+      
+    </div>
+    <footer className="bg-brandBlue shadow dark:bg-brandBlue m-0 mt-20">
+    <div className="w-full mx-auto md:py-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+            <a href="/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                <img src={logo} className="w-12" alt="Flowbite Logo" />
+            </a>
+            <div className="flex justify-center w-full">
+                <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400 -ml-14">
+                    <li>
+                        <a href="/" className="hover:underline me-4 md:me-6">About</a>
+                    </li>
+                    <li>
+                        <a href="/" className="hover:underline me-4 md:me-6">Privacy Policy</a>
+                    </li>
+                    <li>
+                        <a href="/" className="hover:underline me-4 md:me-6">Licensing</a>
+                    </li>
+                    <li>
+                        <a href="/" className="hover:underline">Contact</a>
+                    </li>
+                </ul>
+            </div>
         </div>
-      </footer>
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="/" className="hover:underline">Rehla™</a>. All Rights Reserved.</span>
+    </div>
+</footer>
     </div>
   );
 };
--- client/src/components/MyEvents.js
+++ client/src/components/MyEvents.js
@@ -1,17 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { Link ,useNavigate} from 'react-router-dom';
-import { getAttendedItineraries, getAttendedActivities, rateActivity, commentOnEvent, commentOnItinerary, rateItinerary } from '../services/api'; // Adjust the import based on your file structure
+import { Link, useNavigate } from 'react-router-dom';
+import { getAttendedItineraries, getAttendedActivities } from '../services/api'; // Adjust the import based on your file structure
 import logo from '../images/logo.png';
 
 const MyEvents = () => {
     const [attendedItineraries, setAttendedItineraries] = useState([]);
     const [attendedActivities, setAttendedActivities] = useState([]);
     const [email, setEmail] = useState(''); 
-    const [activityRatings, setActivityRatings] = useState({});
-    const [activityComments, setActivityComments] = useState({});
-    const [itineraryRatings, setItineraryRatings] = useState({});
-    const [itineraryComments, setItineraryComments] = useState({});
     const navigate = useNavigate();
+
     useEffect(() => {
         const storedEmail = localStorage.getItem('email');
         if (storedEmail) {
@@ -35,52 +32,12 @@ const MyEvents = () => {
         fetchData();
     }, [email]);
 
-    const handleRatingChange = (setRatingState, name, rating) => {
-        setRatingState((prevRatings) => ({
-            ...prevRatings,
-            [name]: rating
-        }));
-    };
-
-    const handleCommentChange = (setCommentState, name, comment) => {
-        setCommentState((prevComments) => ({
-            ...prevComments,
-            [name]: comment
-        }));
-    };
-
-    const handleRate = async (rateFunction, name, ratingState) => {
-        const rating = ratingState[name];
-        if (rating) {
-            try {
-                await rateFunction(email, name, rating);
-                alert('Rating submitted successfully!');
-            } catch (error) {
-                console.error('Error rating:', error);
-            }
-        } else {
-            alert('Please enter a rating.');
-        }
-    };
-
-    const handleComment = async (commentFunction, name, commentState) => {
-        const comment = commentState[name];
-        if (comment) {
-            try {
-                await commentFunction(email, name, comment);
-                alert('Comment submitted successfully!');
-            } catch (error) {
-                console.error('Error commenting:', error);
-            }
-        } else {
-            alert('Please enter a comment.');
-        }
-    };
-
-    const handleEventClick = (name, type) => {
-        localStorage.setItem('selectedName', name); // Save the name in local storage
-        localStorage.setItem('selectedType', type); // Save the type (e.g., "itinerary" or "activity") in local storage
-        navigate(`/event-details/${type}/${name}`); // Redirect to the new component
+    const handleEventClick = (name, type, attendedStatus) => {
+        // Save the name, type, and attended status in local storage
+        localStorage.setItem('selectedName', name); 
+        localStorage.setItem('selectedType', type); 
+        localStorage.setItem('attendedStatus', attendedStatus); // Save attended status
+        navigate(`/event-details/${type}/${name}`); // Redirect to the event details page
     };
 
     return (
@@ -102,10 +59,10 @@ const MyEvents = () => {
                 <h2 className="text-xl font-semibold mb-4">Itineraries</h2>
                 <ul className="flex flex-wrap list-none">
                     {attendedItineraries.map(itinerary => (
-                            <li
+                        <li
                             key={itinerary.Itinerary_Name}
                             className="flex items-start mb-4 w-1/2 p-2 cursor-pointer"
-                            onClick={() => handleEventClick(itinerary.Itinerary_Name, 'itinerary')}
+                            onClick={() => handleEventClick(itinerary.Itinerary_Name, 'itinerary', itinerary.Attended)}
                         >
                             <div className="flex items-start bg-gray-100 p-4 rounded-lg shadow-md w-full">
                                 <img
@@ -119,36 +76,6 @@ const MyEvents = () => {
                                         {itinerary.Attended ? ' Attended' : ' Not Attended'}
                                     </span>
                                 </div>
-                                {itinerary.Attended && (
-                                    <div className="flex flex-col ml-4">
-                                        <input
-                                            type="number"
-                                            min="1"
-                                            max="5"
-                                            placeholder="Rate (1-5)"
-                                            className="border border-gray-300 rounded-md p-2 mb-2"
-                                            onChange={(e) => handleRatingChange(setItineraryRatings, itinerary.Itinerary_Name, e.target.value)}
-                                        />
-                                        <input
-                                            type="text"
-                                            placeholder="Comment"
-                                            className="border border-gray-300 rounded-md p-2 mb-2"
-                                            onChange={(e) => handleCommentChange(setItineraryComments, itinerary.Itinerary_Name, e.target.value)}
-                                        />
-                                        <button
-                                            onClick={() => handleRate(rateItinerary, itinerary.Itinerary_Name, itineraryRatings)}
-                                            className="bg-brandBlue text-white font-semibold py-1 px-3 rounded-md hover:bg-blue-600 transition"
-                                        >
-                                            Submit Rating
-                                        </button>
-                                        <button
-                                            onClick={() => handleComment(commentOnItinerary, itinerary.Itinerary_Name, itineraryComments)}
-                                            className="bg-logoOrange text-white font-semibold py-1 px-3 rounded-md hover:bg-green-600 transition mt-2"
-                                        >
-                                            Submit Comment
-                                        </button>
-                                    </div>
-                                )}
                             </div>
                         </li>
                     ))}
@@ -160,10 +87,10 @@ const MyEvents = () => {
                 <h2 className="text-xl font-semibold mb-4">Activities</h2>
                 <ul className="flex flex-wrap list-none">
                     {attendedActivities.map(activity => (
-                            <li
+                        <li
                             key={activity.Activity_Name}
                             className="flex items-start mb-4 w-1/2 p-2 cursor-pointer"
-                            onClick={() => handleEventClick(activity.Activity_Name, 'activity')}
+                            onClick={() => handleEventClick(activity.Activity_Name, 'activity', activity.Attended)}
                         >
                             <div className="flex items-start bg-gray-100 p-4 rounded-lg shadow-md w-full">
                                 <img
@@ -177,36 +104,6 @@ const MyEvents = () => {
                                         {activity.Attended ? ' Attended' : ' Not Attended'}
                                     </span>
                                 </div>
-                                {activity.Attended && (
-                                    <div className="flex flex-col ml-4">
-                                        <input
-                                            type="number"
-                                            min="1"
-                                            max="5"
-                                            placeholder="Rate (1-5)"
-                                            className="border border-gray-300 rounded-md p-2 mb-2"
-                                            onChange={(e) => handleRatingChange(setActivityRatings, activity.Activity_Name, e.target.value)}
-                                        />
-                                        <input
-                                            type="text"
-                                            placeholder="Comment"
-                                            className="border border-gray-300 rounded-md p-2 mb-2"
-                                            onChange={(e) => handleCommentChange(setActivityComments, activity.Activity_Name, e.target.value)}
-                                        />
-                                        <button
-                                            onClick={() => handleRate(rateActivity, activity.Activity_Name, activityRatings)}
-                                            className="bg-brandBlue text-white font-semibold py-1 px-3 rounded-md hover:bg-blue-600 transition"
-                                        >
-                                            Submit Rating
-                                        </button>
-                                        <button
-                                            onClick={() => handleComment(commentOnEvent, activity.Activity_Name, activityComments)}
-                                            className="bg-logoOrange text-white font-semibold py-1 px-3 rounded-md hover:bg-green-600 transition mt-2"
-                                        >
-                                            Submit Comment
-                                        </button>
-                                    </div>
-                                )}
                             </div>
                         </li>
                     ))}
